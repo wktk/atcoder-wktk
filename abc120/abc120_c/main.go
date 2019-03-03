d52bc63e23e864e331021bbca9b8b58e91a72d05
k.wakitani <k.wakitani@gmail.com>
2019-03-03 23:31:27 +0900

    wip: count the number of zero

--- abc120/abc120_c/main.go
+++ abc120/abc120_c/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -14,63 +15,14 @@ func line() string {
 }
 
 func main() {
+	var zero float64
 	input := line()
-	cand := []string{input}
-
-	for q := []string{input}; len(q) > 0; {
-		next := q[0]
-		q = q[1:]
-		for _, index := range avail(next) {
-			did := step(next, index)
-			if !contains(cand, did) {
-				cand = append(cand, did)
-				q = append(q, did)
-			}
+	x := float64(len(input))
+	for i := 0; i < len(input); i++ {
+		if string(input[i]) == "0" {
+			zero++
 		}
 	}
-
-	last := cand[0]
-	for _, c := range cand[1:] {
-		if len(last) > len(c) {
-			last = c
-		}
-	}
-	fmt.Printf("%d\n", len(input)-len(last))
-}
-
-func step(input string, index int) string {
-	var bef string
-	var aft string
-	if index > 0 {
-		bef = input[:index]
-	} else {
-		bef = ""
-	}
-	if index > len(input)-3 {
-		aft = ""
-	} else {
-		aft = input[index+2:]
-	}
-	return bef + aft
-}
-
-func avail(input string) []int {
-	var res []int
-
-	for i := 0; i < len(input)-1; i++ {
-		if input[i] != input[i+1] {
-			res = append(res, i)
-		}
-	}
-
-	return res
-}
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
+	min := math.Min(zero, x-zero)
+	fmt.Printf("%d", int(min*2))
 }
